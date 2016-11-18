70eb4deae1ceda34b498084c415567cc6d7f8992
Cecelia Auerswald <cecelia.auerswald@gmail.com>
2016-11-18 10:34:12 -0500

    adding in reciever control and writer control code

---
+++ receiver_control/receiver_control.ino
@@ -0,0 +1,92 @@
+#include <Wire.h>
+
+bool is_int = false; //stands for int
+
+char c;
+int i;
+
+int soilSensorVal1;
+int soilSensorPin1 = A0;
+
+int soilSensorVal2;
+int soilSensorPin2 = A1;
+
+int soilSensorVal3;
+int soilSensorPin3 = A2;
+
+int soilSensorVal4;
+int soilSensorPin4 = A3;
+
+int lightSensorVal1;
+int lightSensorVal2;
+int lightSensorVal3;
+int lightSensorVal4;
+
+int pumpPin = 8;
+
+void setup() {
+  // put your setup code here, to run once:
+  Wire.begin(8);
+  Wire.onReceive(receiveEvent);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  soilSensorVal1 = analogRead(soilSensorPin1);
+  soilSensorVal2 = analogRead(soilSensorPin1);
+  soilSensorVal3 = analogRead(soilSensorPin1);
+  soilSensorVal4 = analogRead(soilSensorPin1);
+
+  if ((soilSensorVal1 + soilSensorVal2 + soilSensorVal3 + soilSensorVal4)/4 < 800) {
+    digitalWrite(pumpPin, HIGH);   // sets the LED on
+  } else {
+    digitalWrite(pumpPin, LOW);   // sets the LED on
+  }
+
+  
+  //Serial.println("START");
+  Serial.print(lightSensorVal1);
+  Serial.print("/");
+  Serial.println(soilSensorVal1);
+  Serial.print("/");
+  Serial.print(lightSensorVal2);
+  Serial.print("/");
+  Serial.println(soilSensorVal2);
+  Serial.print("/");
+  Serial.print(lightSensorVal3);
+  Serial.print("/");
+  Serial.println(soilSensorVal3);
+  Serial.print("/");
+  Serial.print(lightSensorVal4);
+  Serial.print("/");
+  Serial.println(soilSensorVal4);
+  //Serial.println("END");
+}
+
+void receiveEvent(int howMany) {
+  while(Wire.available()) {
+    if (Wire.available()%2 == 1) { 
+      i = Wire.read();
+      //Serial.print(i);
+      if (c == "a") {
+        lightSensorVal1 = i;
+      }
+      else if (c == "b") {
+        lightSensorVal2 = i;
+      }
+      else if (c == "c") {
+        lightSensorVal3 = i;
+      }
+      else if (c == "d") {
+        lightSensorVal4 = i;
+      }
+    } else {
+      c = Wire.read();
+
+    }
+  }
+}
+
+
+
---
+++ writer_control/writer_control.ino
@@ -0,0 +1,92 @@
+#include <Wire.h>
+#include <Adafruit_NeoPixel.h>
+
+#define PIN     10
+#define N_LEDS  20
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(N_LEDS, PIN, NEO_GRB + NEO_KHZ800);
+
+int lightSensorVal1;
+int lightSensorPin1 = A0;
+
+int lightSensorVal2;
+int lightSensorPin2 = A1;
+
+int lightSensorVal3;
+int lightSensorPin3 = A2;
+//
+int lightSensorVal4;
+int lightSensorPin4 = A3;
+
+int num1 = N_LEDS / 4;
+int num2 = (2 * N_LEDS) / 4;
+int num3 = (3 * N_LEDS) / 4;
+int num4 = (4 * N_LEDS) / 4;
+
+void setup() {
+  // put your setup code here, to run once:
+  Wire.begin();
+  strip.begin();
+}
+
+void loop() {
+  readLights();
+//  controlLights();
+  sendDatatoReceiver();
+}
+
+void sendDatatoReceiver() {
+  Wire.beginTransmission(8);
+  Wire.write("a");
+  Wire.write(lightSensorVal1);
+  Wire.write("b");
+  Wire.write(lightSensorVal2);
+  Wire.write("c");
+  Wire.write(lightSensorVal3);
+  Wire.write("d");
+  Wire.write(lightSensorVal4);
+  Wire.endTransmission();
+
+  delay(100);
+}
+
+void readLights() {
+  lightSensorVal1 = analogRead(lightSensorPin1);
+//  lightSensorVal2 = analogRead(lightSensorPin2);
+//  lightSensorVal3 = analogRead(lightSensorPin3);
+//  lightSensorVal4 = analogRead(lightSensorPin4);
+}
+
+void controlLights() {
+  if (lightSensorVal1 < 950) {
+    chase(strip.Color(255, 255, 255), 0, num1); // Red
+  } else {
+    chase(strip.Color(0, 0, 0),0, num1);
+  }
+
+//  if (lightSensorVal2 < 950) {
+//    chase(strip.Color(255, 255, 255), num1, num2); // Red
+//  } else {
+//    chase(strip.Color(0, 0, 0), num1, num2);
+//  }
+
+//  if (lightSensorVal3 < 950) {
+//    chase(strip.Color(255, 255, 255), num2, num3); // Red
+//  } else {
+//    chase(strip.Color(0, 0, 0), num2, num3);
+//  }
+//
+//  if (lightSensorVal4 < 950) {
+//    chase(strip.Color(255, 255, 255), num4, N_LEDS); // Red
+//  } else {
+//    chase(strip.Color(0, 0, 0), num4, N_LEDS);
+//  }
+}
+
+static void chase(uint32_t c, int numstart, int numend) {
+  for(uint16_t i=numstart; i<numend; i++) {
+      strip.setPixelColor(i  , c); // Draw new pixel
+      strip.show();
+  }
+} 
+
