e6f2708f9a71b990b166e9f7c70cc5d14b59a94e
Aditi Joshi <aditijoshi@AditisMacintosh.home>
2016-12-11 16:36:42 -0500

    pushing changes to website and gui

--- GardenaiApp.py
+++ GardenaiApp.py
@@ -17,28 +17,37 @@ from kivy.uix.textinput import TextInput
 from garden import Garden
 
 
-
 class GardenaiGame(Widget):
+
     our_garden = ObjectProperty(None)
     our_garden = Garden(4,["Tom Gore", "Mary Jane", "Marvin", "December"], ["Thyme", "Cilantro", "Thyme", "Cilantro"])
 
+
     # FOR TESTING WHEN WE ARE NOT HOOKED UP TO ARDUINO
-    # for plant in our_garden.plants:
-    #     # this is for testing
-    #     plant.soil_vals = [300,400,500,600]
-    #     plant.light_vals = [800,900,1000,1100]
-    #     plant.get_soil_state()
-    #     plant.get_light_state()
+    for plant in our_garden.plants:
+        # this is for testing
+        plant.soil_vals = [300,400,500,600]
+        plant.light_vals = [800,900,1000,1100]
+        plant.get_soil_state()
+        plant.get_light_state()
 
     # UNCOMMENT THIS LINE WHEN WE ARE HOOKED UP TO ARDUINO
-    our_garden.collect_data()
+    #our_garden.collect_data()
 
 
 class GardenaiApp(App):
 
     def build(self):
-        textinput = TextInput(text='Hello world')
-        return GardenaiGame()
+        w = GardenaiGame()
+        w.plant0name = w.our_garden.plants[0].name
+        w.plant1name = w.our_garden.plants[1].name
+        w.plant2name = w.our_garden.plants[2].name
+        w.plant3name = w.our_garden.plants[3].name
+        w.plant0type = w.our_garden.plants[0].herb
+        w.plant1type = w.our_garden.plants[1].herb
+        w.plant2type = w.our_garden.plants[2].herb
+        w.plant3type = w.our_garden.plants[3].herb
+        return w
 
 
 if __name__ == '__main__':
